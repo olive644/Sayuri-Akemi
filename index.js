3aebe28e0258f7cd59c453149793c1bccf8fa036
Tomoko San <63744581+tomokoSan20@users.noreply.github.com>
2020-09-14 18:27:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 
+require('dotenv').config();
+
 const client = new Discord.Client();
+
+const prefix = process.env.PREFIX;
 
 client.once('ready', () => {
 	console.log('Yey! Estou Pronta.');
